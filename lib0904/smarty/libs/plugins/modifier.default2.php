523822dbb7da08b47b94b141a40d220b2d4886a0
近藤清志 <daigorou02060905flyfisher@gmail.com>
2016-11-22 13:54:28 +0900

    insert時の文字コード変換

--- lib0904/smarty/libs/plugins/modifier.default2.php
+++ lib0904/smarty/libs/plugins/modifier.default2.php
@@ -10,16 +10,16 @@
  * Smarty default modifier plugin
  *
  * Type:     modifier<br>
- * Name:     default<br>
+ * Name:     default2<br>
  * Purpose:  designate default value for empty variables
- * @link http://smarty.php.net/manual/en/language.modifier.default.php
+ * @link http://smarty.php.net/manual/en/language.modifier.default2.php
  *          default (Smarty online manual)
  * @author   Monte Ohrt <monte at ohrt dot com>
  * @param string
  * @param string
  * @return string
  */
-function smarty_modifier_default($string, $default = '')
+function smarty_modifier_default2($string, $default = '')
 {
     if (!isset($string) || $string === '')
         return $default;
