be226b0ee25a025559cd7647ee70bb4616ecbf87
yasushiyoshida <ysdyss0617@yahoo.co.jp>
2017-02-17 15:55:31 +0900

    -

--- public_html/artist/kubota/login/bbcm_tkm121/top.php
+++ public_html/artist/kubota/login/bbcm_tkm121/top.php
@@ -3,10 +3,10 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/../lib0904/simple/DefaultAction.php')
 
 class Action extends DefaultAction {
 	function validate() {
-		if(!$_SESSION["loginID"]['normal']) {
-			$this->__controller->redirectToURL('/artist/kubota/login/');
-			exit();
-		}
+		// if(!$_SESSION["loginID"]['normal']) {
+		// 	$this->__controller->redirectToURL('/artist/kubota/login/');
+		// 	exit();
+		// }
 
 		return true;
 	}
